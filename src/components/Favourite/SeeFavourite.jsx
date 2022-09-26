f3d9e97424acc78a08082d069df2c72ec87cbf6c
JaMeS-18-18 <chitak1818@gmail.com>
2022-09-26 14:00:29 +0500

    All worked

--- src/components/Favourite/SeeFavourite.jsx
+++ src/components/Favourite/SeeFavourite.jsx
@@ -13,7 +13,7 @@ const SeeFavourite = () => {
                             
                             <div className="col-12 my-2">
                                 <div className="cardd d-flex  align-items-center shadow">
-                                    <img className='img-fluid imgages rounded' src={`https://image.tmdb.org/t/p/w500/` + item.poster_path} alt="" />
+                                    <img className=' imgages rounded' src={`https://image.tmdb.org/t/p/w500/` + item.poster_path} alt="" />
                                     <div className='mx-1s'>
                                         <h3>{item.name}</h3>
                                         <p>{item.first_air_date}</p>
